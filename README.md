cbe5befc8a481fc8383749795a5812dfa031feac
Artlfmj <arthurdepenanster@gmail.com>
2021-09-19 14:06:14 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # simple-discord-fn-stats
- 
+![Visitors](https://visitor-badge.laobi.icu/badge?page_id=Artlfmj.simple-discord-fn-stats) 
+
+## What is Simple Discord Fn Stats 
+As it's name says, this bot is a very simple usage. **Show Fortnite stats** . It was designed to help [Noah](https://twitter.com/OpsweatC) and [Assassin](https://twitter.com/AssassinBTWW) get their stats as well as their friends stats.
+
+# How to use this bot?
+If you are new to programming and don't know how to use this bot this is a step by step tutorial on how to use it :
+
+- If you haven't already Node.js installed install it. [Download here](https://nodejs.org/dist/v16.9.1/node-v16.9.1-x64.msi), then run the installer. Once done reboot your pc. Then do windows r. A little app has just opened. Type cmd and enter. <br/> 
+Then there are 2 options : 
+- A command prompt has just opened. Type node -v to check the node installation. If you get an error reinstall node. Now open the folder in which the bot is by doing: cd [the path] . To find the path open your folder properties and copy the path to paste it in the cmd. 
+- Run npm i in the cmd. Once done check in the folder that a node_modules folder has appeared.
+
+- Run install.bat
+
+Then
+
+- Open the env.example folder . Fill the token lign with a Discord Bot token (Obtainable on https://discord.com/developers/applications) If you are struggling with this part please look at [this article](https://discordpy.readthedocs.io/en/stable/discord.html#)
+- Rename env.example to .env
+
+- Once all done either do node . in your command shell or run run.bat
+
+### If you have any questions or errors please open an [issue](../../issues) or dm on Discord
+
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const client = new Discord.Client({
 client.on("ready", async() => {
     console.log(chalk.green(`Successfuly connected to ${client.user.tag}`))
     console.warn(chalk.bold("Registering slash commands"))
+    // Registering stats command
     const stats = {
         name : "stats",
         description : "Gets stats of a Fortnite Player",
@@ -65,7 +66,70 @@ client.on("ready", async() => {
     .then(console.log(chalk.green("Stats command | Status : Registered")))
 
     console.log(chalk.yellow.bold("IF THIS IS THE FIRST TIME USING THIS BOT YOU MAY NEED TO WAIT UP TO 15 MIN TO SEE THE SLASH COMMADS ON YOUR SERVER. IF YOU WANT TO SPEED THE PROCESS, PLEASE ADD THE BOT AFTER HAVING RUN AT LEAST ONE TIME THE SETUP PROCESS"))
-    
+
+})
+client.on('interactionCreate', async(interaction) => {
+    if(interaction.commandName === "stats"){
+        await interaction.reply({content : "Loading stats"})
+        // Stats command
+        const username = interaction.options.getString("username")
+        let platform;
+        if(interaction.options.getString("platform")){
+            platform = interaction.options.getString("platform")
+        } else { platform = "epic"}
+        let timeWindow;
+        if(interaction.options.getString("timewindow")){
+            timeWindow = interaction.options.getString("timewindow")
+        } else { timeWindow = "lifetime"}
+        const req = await axios({
+            url : "https://fortnite-api.com/v2/stats/br/v2",
+            method : "get",
+            headers : {
+                "x-api-key" : process.env.KEY
+            },
+            params : {
+                name : username,
+                accountType : platform,
+                timeWindow : timeWindow,
+                image : "all"
+            }
+        })
+        .catch(e => {
+            const em = new Discord.MessageEmbed()
+            .setColor("RED")
+            .setTitle(e.toJSON().name)
+            .setDescription("An error occured, multiple reasons are possible: \n404 : The account you provided does not exist\n403 : The account you provided hasn't set his stats to public\n400 : Please retry later")
+            .setFooter(e.toJSON().message)
+            return interaction.editReply({embeds : [em]})
+        })
+        if(req){
+           
+            const skins = await axios({
+                method : "get",
+                url : "https://fortnite-api.com/v2/cosmetics/br/search/all",
+                params : {
+                    "type" : "outfit"
+                }
+            })
+           
+            const embed = new Discord.MessageEmbed()
+            .setAuthor(username, skins.data.data[getRandomInt(skins.data.data.length - 1)].images.icon)
+            .setTitle("Additional data")
+            .addField('Account ID', req.data.data.account.id)
+            .addField("Acccount Name", req.data.data.account.name, true)
+            .addField("Battlepass Level", req.data.data.battlePass.level.toString())
+            .setImage(`${req.data.data.image}`)
+            .setColor("RANDOM")
+            interaction.editReply({embeds : [embed], content : "\u200b"})
+            .catch(e => {
+                console.log(e)
+            })
+        }
+
+    }
 })
 
 client.login(process.env.TOKEN)
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
